b72fe097d146cabd9884513347584eb0386ebf2c
AnyumbaKE <esanyumba@gmail.com>
2024-04-11 02:25:40 +0300

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,4 @@
-#!/usr/bin/env node
-
-export default function getResponseFromAPI () {
-  return new Promise((resolve, reject) => {
-    resolve('Hi Mom');
-    // Rejecting with an Error object
-    reject(new Error('Rejected'));
-  });
+/* eslint-disable */
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
 }
