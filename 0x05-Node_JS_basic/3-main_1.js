b12e698b5864610e663d3aebbdef22f4b7881684
AnyumbaKE <esanyumba@gmail.com>
2024-06-04 12:12:11 -0700

    3. Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-main_1.js
@@ -0,0 +1,10 @@
+onst countStudents = require('./3-read_file_async');
+
+countStudents("database.csv")
+    .then(() => {
+        console.log("Done!");
+    })
+        .catch((error) => {
+        console.log(error);
+    });
+console.log("After!");
